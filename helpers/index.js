91ad234092e7f310ba917207714042be5419cfbf
Dhiya <jaradidia@yahoo.com>
2019-07-22 21:57:09 -0700

    update pass for reset

--- helpers/index.js
+++ helpers/index.js
@@ -9,8 +9,8 @@ exports.sendEmail = emailData => {
         secure: false,
         requireTLS: true,
         auth: {
-            user: "masterjupiter2015@gmail.com",
-            pass: "kshzlmomlthllktq"
+            user: "daljaradi@gmail.com",
+            pass: "pdwsnzcxkxaynxsn"
         }
     });
     return transporter
